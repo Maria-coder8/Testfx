f9c7f95423c41c5ece24f36b45d826da0a93fc77
clara004 <claramoussa111@gmail.com>
2024-12-11 15:34:36 +0200

    gui updated

--- src/main/java/com/example/testfx/Step2Cobtroller.java
+++ src/main/java/com/example/testfx/Step2Cobtroller.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,6 +20,16 @@ public class Step2Cobtroller {
         Main.stage.show();
 
     }
+
+    @FXML
+    private ToggleButton btnStandard;
+    @FXML
+    private ToggleButton btnPremium;
+    @FXML
+    private ToggleButton btnTelecom;
+
+
+
     @FXML
     protected void BacktoStep1() throws IOException{
         FXMLLoader fxmlLoader=new FXMLLoader(Step2Cobtroller.class.getResource("Register.fxml"));
